a391f65d46004d38b73289aebdf0f13242d213e5
saurabhchobey <52131942+saurabhchobey@users.noreply.github.com>
2021-05-13 10:55:00 +0530

    Add files via upload

---
+++ src/components/elements/detail.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import contact from '../contacts/Contact'
+import contacts from '../contacts/Contacts'
+
+const detail = () => {
+    return (
+       <>
+        <div>
+               DETAILS
+           </div>
+       <div className='card'>
+          
+           <table>
+       <th>Name</th>
+            <th>Phone</th>
+            <th>E-mail</th>
+            <th>Actions</th>
+            </table>
+       </div>
+       </>
+    )
+}
+
+export default detail
---
+++ src/components/elements/favor.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const favor = () => {
+    return (
+        <>
+        <div>
+            FOVORITE CONTACTS
+            </div>
+            <div className='card'>
+           <table>
+       <th>Name</th>
+            <th>Phone</th>
+            <th>E-mail</th>
+            <th>Actions</th>
+            </table>
+       </div>
+            
+        </>
+    )
+}
+
+export default favor
